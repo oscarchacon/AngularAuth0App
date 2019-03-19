3ebb9797a1c5be0ed601e29860b55fe65933a70a
Oscar Chacon <oscarchaconcautin@gmail.com>
2019-03-19 17:06:56 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
+  entryComponents: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
