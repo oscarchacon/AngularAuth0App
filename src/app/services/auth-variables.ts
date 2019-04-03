80e02946488ae69e15e8c3a96ce912d2ad80987f
Oscar Chacon <oscarchaconcautin@gmail.com>
2019-04-03 13:43:28 -0300

    Fix Cambios

--- src/app/services/auth-variables.ts
+++ src/app/services/auth-variables.ts
@@ -1,8 +1,9 @@
 export const AUTH_VAR = {
   auth : {
-    clientID: 'caQdQv3w4e9ecnnI6d4CHzdE2kgqo3fB',
+    //clientID: 'caQdQv3w4e9ecnnI6d4CHzdE2kgqo3fB',
+    clientID: 'waFnr4jsBURpqUakil0aD9Fb7yS084eG',
     domain: 'oscarchacon.auth0.com', // e.g., you.auth0.com
-    //audience: '', // e.g., http://localhost:3001
+    audience: 'BackJWTAuth0Core', // e.g., http://localhost:3001
     redirectUri: 'http://localhost:4200/callback',
     logoutUrl: 'http://localhost:4200',
     scope: 'openid profile email'
