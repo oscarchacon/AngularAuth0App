08887147574b5837b81c246824aff272a8b70e6c
Oscar Chacon <oscarchaconcautin@gmail.com>
2019-04-05 17:21:49 -0300

    Integracion con branch

--- src/app/services/auth-variables.ts
+++ src/app/services/auth-variables.ts
@@ -3,8 +3,9 @@ export const AUTH_VAR = {
     //clientID: 'caQdQv3w4e9ecnnI6d4CHzdE2kgqo3fB',
     clientID: 'waFnr4jsBURpqUakil0aD9Fb7yS084eG',
     domain: 'oscarchacon.auth0.com', // e.g., you.auth0.com
-    audience: 'BackJWTAuth0Core', // e.g., http://localhost:3001
+    //audience: 'BackJWTAuth0Core', // e.g., http://localhost:3001
     redirectUri: 'http://localhost:4200/callback',
+    apiURL: 'https://localhost:5001',
     logoutUrl: 'http://localhost:4200',
     scope: 'openid profile email'
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import * as auth0 from 'auth0-js';
 import { AUTH_VAR } from './auth-variables';
+import { Observer, Observable, timer } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -14,16 +15,22 @@ export class AuthService {
     clientID: AUTH_VAR.auth.clientID,
     domain: AUTH_VAR.auth.domain,
     responseType: 'token id_token',
+    audience: AUTH_VAR.auth.apiURL,
     redirectUri: AUTH_VAR.auth.redirectUri,
     scope: AUTH_VAR.auth.scope
   });
 
   userProfile: any;
+  refreshSubscription: any;
+  observer: Observer<boolean>;
+  ssoAuthComplete$: Observable<boolean> = new Observable(
+    obs => (this.observer = obs)
+  );
 
   constructor(public router: Router) {
-    this._idToken = '';
+    /*this._idToken = '';
     this._accessToken = '';
-    this._expiresAt = 0;
+    this._expiresAt = 0;*/
   }
 
   get accessToken(): string {
@@ -41,7 +48,7 @@ export class AuthService {
   public handleAuthentication(): void {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = '';
+        //window.location.hash = '';
         this.localLogin(authResult);
         //this.router.navigate(['/home']);
         this.router.navigate(['/']);
@@ -53,17 +60,21 @@ export class AuthService {
     });
   }
 
+
   private localLogin(authResult): void {
     // Set isLoggedIn flag in localStorage
     localStorage.setItem('isLoggedIn', 'true');
     // Set the time that the access token will expire at
     const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
     this._accessToken = authResult.accessToken;
+    localStorage.setItem('access_token', this._accessToken);
     this._idToken = authResult.idToken;
+    localStorage.setItem('id_token', this._idToken);
     this._expiresAt = expiresAt;
+    localStorage.setItem('expires_at', JSON.stringify(this._expiresAt));
   }
 
-  public renewTokens(): void {
+  /*public renewTokens(): void {
     this.auth0.checkSession({}, (err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.localLogin(authResult);
@@ -72,6 +83,22 @@ export class AuthService {
         this.logout();
       }
     });
+  }*/
+
+  public renewToken() {
+    this.auth0.checkSession({},
+      (err, result) => {
+        if (err) {
+          alert(
+            `Could not get a new token (${err.error}: ${err.error_description}).`
+          );
+          this.login();
+        } else {
+          this.localLogin(result);
+          this.observer.next(true);
+        }
+      }
+    );
   }
 
   public logout(): void {
@@ -81,6 +108,9 @@ export class AuthService {
     this._expiresAt = 0;
     // Remove isLoggedIn flag from localStorage
     localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
     localStorage.clear();
     this.auth0.logout({
       returnTo: AUTH_VAR.auth.logoutUrl,
@@ -97,6 +127,7 @@ export class AuthService {
   }
 
   public getProfile(cb): void {
+    this._accessToken = localStorage.getItem('access_token');
     if (!this._accessToken) {
       throw new Error('Access Token must exist to fetch profile');
     }
@@ -108,6 +139,34 @@ export class AuthService {
       }
       cb(err, profile);
     });
+  }
+
+  public scheduleRenewal() {
+    if (!this.isAuthenticated()) { return; }
+    this.unscheduleRenewal();
+
+    const expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));
+
+    const source = Observable.of(expiresAt).flatMap(expiresAt => {
+      const now = Date.now();
+
+      // Use the delay in a timer to
+      // run the refresh at the proper time
+      return timer(Math.max(1, expiresAt - now));
+    });
+
+    // Once the delay time from above is
+    // reached, get a new JWT and schedule
+    // additional refreshes
+    this.refreshSubscription = source.subscribe(() => {
+      this.renewToken();
+      this.scheduleRenewal();
+    });
+  }
+
+  public unscheduleRenewal() {
+    if (!this.refreshSubscription) { return; }
+    this.refreshSubscription.unsubscribe();
   }
 
 }
